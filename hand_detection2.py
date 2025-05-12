3908f4e0021fc879cb8b14716b7d8675d66ff97b
Aditya Chaudhary <148548829+AdiCh10@users.noreply.github.com>
2025-05-12 17:38:02 +0530

    Add files via upload

---
+++ hand_detection2.py
@@ -0,0 +1,28 @@
+#Importing the libraries
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mphands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+hands = mphands.Hands()
+while True:
+    data,image = cap.read()
+    if not data:
+        break
+    #Flip the Image
+    image = cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+    #storing the results
+    results = hands.process(image)
+    image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,mphands.HAND_CONNECTIONS)
+    cv2.imshow('Handtracker',image)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        print("Exiting...")
+        break
